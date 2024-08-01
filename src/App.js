7ab5284acbf38877c2452b293872ab2b9684f3ba
glesu <glesu@naver.com>
2024-08-01 15:13:01 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '눙눙이의 하루2';
+  let [a,b] = useState('남자 코트 추천');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="black-nav">
+        <h4 style = {{color : 'White' , fontSize : '30px'}}>React Study Blog</h4>
+      </div>
+      <div className="list">
+          <h4>{a}</h4>
+          <p>08월 01일 발행</p>
+      </div>
+      <h4>{post} 후후</h4>
     </div>
   );
 }
