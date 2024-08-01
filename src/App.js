3ec0e9b1a35a4080efbba6a84ae42ad18faff01d
glesu <glesu@naver.com>
2024-08-01 15:38:47 +0900

    hw 1.4

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '눙눙이의 하루2';
-  let [a,b] = useState('남자 코트 추천');
+  let post = '눙눙이의 하루3';
+  let [title,set_tltle] = useState(['햄스토리','햄파이어','햄파이트']);
   
   return (
     <div className="App">
@@ -13,10 +13,19 @@ function App() {
         <h4 style = {{color : 'White' , fontSize : '30px'}}>React Study Blog</h4>
       </div>
       <div className="list">
-          <h4>{a}</h4>
-          <p>08월 01일 발행</p>
+        <h4>{title[0]}</h4>
+        <p>240801</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>240801</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>240801</p>
       </div>
       <h4>{post} 후후</h4>
+      
     </div>
   );
 }
